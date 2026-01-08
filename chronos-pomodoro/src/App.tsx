e886ac0779997e9479fca4c3667c993208d1c84d
Wesley Breno <wesleybreno074@gmail.com>
2026-01-08 12:58:38 -0300

    Learning about prop typing and adjusting the CSS of the heading component.

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,18 +1,18 @@
+import { TimerIcon } from 'lucide-react';
 import { Heading } from './components/Heading';
 
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-    console.log('oi')
-
     return (
         <>
-            <Heading attr={123} attr2='String'>
+            <Heading>
                 Olá mundo!
+                <button>
+                    <TimerIcon />
+                </button>
             </Heading>
-
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, similique mollitia! Expedita officiis quasi debitis iure dolor recusandae magnam adipisci quaerat et nesciunt nihil ad eius repellendus corporis, dolorem qui?</p>
         </>
     )
 }
