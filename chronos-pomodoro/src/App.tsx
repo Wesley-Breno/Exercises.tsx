f692a6e780d9d22db2828000f3e905b4564ede60
wesley <wesleybreno074@gmail.com>
2026-01-21 02:08:53 -0300

    Creating a homepage form

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -20,6 +20,36 @@ export function App() {
             <Container>
                 <CountDown />
             </Container>
+
+            <Container>
+                <form className='form' action="">
+                    <div className="formRow">
+                        <label htmlFor="">Task</label>
+                        <input id="input" type="text"/>
+                    </div>
+
+                    <div className="formRow">
+                        <p>
+                            Lorem ipsum dolor, sit amet
+                        </p>
+                    </div>
+
+                    <div className="formRow">
+                        <p>
+                            Ciclos
+                        </p>
+                        <p>
+                            0 0 0 0 0 0 
+                        </p>
+                    </div>
+
+                    <div className="formRow">
+                        <button type="submit">
+                            Enviar
+                        </button>
+                    </div>
+                </form>
+            </Container>
         </>
     )
 }
