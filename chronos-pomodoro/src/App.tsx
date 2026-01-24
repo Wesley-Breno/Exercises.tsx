1eb95c62b770a4b19e14fbd542c7590a2ff9e1dd
wesley <wesleybreno074@gmail.com>
2026-01-24 20:33:50 -0300

    Creating a reusable button component with options

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,6 +1,8 @@
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 import { Container } from './components/Container';
 import { CountDown } from './components/CountDown';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
 import { DefaultInput } from './components/DefaultInput';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
@@ -46,9 +48,8 @@ export function App() {
                     </div>
 
                     <div className="formRow">
-                        <button type="submit">
-                            Enviar
-                        </button>
+                        <DefaultButton icon={<PlayCircleIcon/>} color='green'/>
+                        <DefaultButton icon={<StopCircleIcon/>} color='red'/>
                     </div>
                 </form>
             </Container>
