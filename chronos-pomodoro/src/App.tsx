124b4c45aaff27a81f27a67c7e993fd89caebf68
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2026-03-05 19:11:48 -0300

    Delete chronos-pomodoro directory

--- chronos-pomodoro/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { Home } from './pages/Home';
-
-import './styles/global.css';
-import './styles/theme.css';
-
-export function App() {
-    return <Home/>
-}
--- chronos-pomodoro/src/components/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css';
-
-
-export function Footer() {
-    return <footer className={styles.footer}>
-        <a href="">Entenda como funciona a tecnica pomodoro</a>
-        <a href="">Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com amor!</a>
-    </footer>
-}
